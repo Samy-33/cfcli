5aeb1dd4c64102d3abdfc1e2f1954358048a21ed
Saket Patel <saket.is.sam@gmail.com>
2018-10-03 03:36:21 +0530

    Added downloading of tests for a problem and testing the output by running executable

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+	name='CodeForces-CLI',
+	version='0.0.1',
+	py_modules=['cli'],
+	install_requires=[
+		'Click',
+		'requests',
+		'bs4'
+	],
+	entry_points='''
+		[console_scripts]
+		cfcli=cli:cli
+	'''
+)
